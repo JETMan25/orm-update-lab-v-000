665a2cef1f0a0ce51614dba8cf052d1aced59940
jontenthoff <jontenthoff@hotmail.com>
2019-05-15 17:36:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,6 +14,13 @@ class Student
     end
   
   def self.create_table
-    
+       CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    )
+    SQL
+
+    DB[:conn].execute(sql)
   end
 end
